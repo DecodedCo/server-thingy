26e294241b2e20f5a68f029a810baa276f2ef686
Joe Dytrych <me@jdytrych.co.uk>
2013-08-29 01:19:18 +0100

    Basic file server

---
+++ little-server.js
@@ -0,0 +1,80 @@
+/* global $, require */
+
+"use strict";
+
+var http = require("http"),
+  connect = require("connect");
+
+var fileDialogue = $(".file-dialogue"),
+  fileLauncher = $(".file-launcher"),
+  serverStopper = $(".server-stopper");
+
+var expandSize = 400,
+  expandInterval = 50;
+
+var server;
+
+function launchServer(dir) {
+  var app = connect();
+
+  app.use(logger);
+
+  // static files:
+  app.use(connect.static(dir));
+
+  // Directory listings:
+  app.use(connect.directory(dir));
+
+  server = http.createServer(app);
+
+  server.listen(1337);
+}
+
+function stopServer() {
+  server.close();
+}
+
+function logger(req, res, next) {
+  // To do: Log request info ala serve
+
+  next();
+}
+
+function expand(contract) {
+  var soFar = 0,
+    interval = contract ? -expandInterval : expandInterval;
+
+  function doExpand () {
+    if (soFar < expandSize) {
+      window.resizeBy(interval, 0);
+      soFar += expandInterval;
+      window.requestAnimationFrame(doExpand);
+    }
+  }
+
+  doExpand();
+}
+
+fileDialogue.on("change", function () {
+  var dir = $(this).val();
+
+  launchServer(dir);
+
+  fileLauncher.css("display", "none");
+  serverStopper.css("display", "block");
+
+  expand(false);
+});
+
+fileLauncher.on("click", function() {
+  fileDialogue.trigger("click");
+});
+
+serverStopper.on("click", function() {
+  stopServer();
+
+  fileLauncher.css("display", "block");
+  serverStopper.css("display", "none");
+
+  expand(true);
+});
