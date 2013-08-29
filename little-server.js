92f4d331481a21399ce5b112f5dd8f2c93ac6d3c
Joe Dytrych <me@jdytrych.co.uk>
2013-08-29 17:06:17 +0100

    Add server logs

--- little-server.js
+++ little-server.js
@@ -6,12 +6,14 @@ var http = require("http"),
   connect = require("connect"),
   open = require("open");
 
-var fileDialogue = $(".file-dialogue"),
-  fileLauncher = $(".file-launcher"),
-  serverStopper = $(".server-stopper");
+var fileLauncher = $(".file-launcher"),
+  serverStopper = $(".server-stopper"),
+  logEl = $("pre");
 
 var expandSize = 400,
   expandInterval = 50;
+
+var logs = [];
 
 var server;
 
@@ -29,21 +31,87 @@ function launchServer(dir) {
   server = http.createServer(app);
 
   server.listen(1337);
+
+  logEl.html("Waiting for a connection...");
 }
 
 function stopServer() {
   server.close();
+  clearLogs(0);
 }
 
 function logger(req, res, next) {
-  // To do: Log request info ala serve
+  var size = res.getHeader("content-length");
+
+  if (size) {
+    size = bytesToString(size);
+  } else {
+    size = "";
+  }
+
+  var time = new Date(Date.now());
+
+  logs.push({
+    time: time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds(),
+    url: req.url,
+    status: res.statusCode,
+    size: size
+  });
+
+  clearLogs(50);
+
+  updateLogs();
 
   next();
+}
+
+function updateLogs() {
+  // TODO: Find a nicer way of doing this:
+  var out = "",
+    log;
+
+  for (var i = 0; i < logs.length; i++) {
+    log = logs[i];
+    out += log.time + " - "
+    out += log.status + " - ";
+    out += log.url + "\n";
+  }
+
+  logEl.html(out);
+  document.body.scrollTop = 10000;
+}
+
+function clearLogs(n) {
+  while (logs.length > n) {
+    logs.shift();
+  }
+}
+
+function bytesToString(b) {
+  var kb = 1024,
+    mb = kb * 1024,
+    gb = mb * 1024;
+
+  if (b > gb) {
+    return (b / gb).toFixed(2) + "GB";
+  } else if (b > mb) {
+    return (b / mb).toFixed(2) + "MB";
+  } else if (b > kb) {
+    return (b / kb).toFixed(2) + "KB";
+  } else {
+    return b + "B";
+  }
 }
 
 function expand(contract) {
   var soFar = 0,
     interval = contract ? -expandInterval : expandInterval;
+
+  if (contract) {
+    $(document.body).removeClass("expanded");
+  } else {
+    $(document.body).addClass("expanded");
+  }
 
   function doExpand () {
     if (soFar < expandSize) {
@@ -56,8 +124,13 @@ function expand(contract) {
   doExpand();
 }
 
-fileDialogue.on("change", function () {
-  var dir = $(this).val();
+$(document).on("change", ".file-dialogue", function () {
+  var dialogue = $(this),
+    dir = dialogue.val();
+
+  if (dir === "DECODEDSERVE_NOOP") {
+    return;
+  }
 
   launchServer(dir);
 
@@ -65,10 +138,12 @@ fileDialogue.on("change", function () {
   serverStopper.css("display", "block");
 
   expand(false);
+
+  dialogue.replaceWith($("<input class=\"file-dialogue\" type=\"file\" nwdirectory>"));
 });
 
 fileLauncher.on("click", function() {
-  fileDialogue.trigger("click");
+  $(".file-dialogue").trigger("click");
 });
 
 serverStopper.on("click", function() {
